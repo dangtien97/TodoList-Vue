3c2a1e8a95ef329a73100d18b08dc1e7e004bb23
dangtien97 <tien.nd97@outlook.com>
2021-04-12 11:02:29 +0700

    Fix vue router

--- src/router.js
+++ src/router.js
@@ -5,35 +5,36 @@ import store from "@/store";
 Vue.use(Router);
 
 const router = new Router({
-  mode: "history",
-  routes: [
-    {
-      path: "/",
-      name: "Home",
-      // lazy-loaded
-      component: () => import("./views/Todo.vue"),
-      meta: {
-        requireAuth: true,
-      },
-    },
-    {
-      path: "/login",
-      name: "Login",
-      component: () => import("./views/Login.vue"),
-    },
-    {
-      path: "/register",
-      name: "Register",
-      component: () => import("./views/Register.vue"),
-    },
-  ],
+	mode: "history",
+	base: "/TodoList-Vue",
+	routes: [
+		{
+			path: "/",
+			name: "Home",
+			// lazy-loaded
+			component: () => import("./views/Todo.vue"),
+			meta: {
+				requireAuth: true,
+			},
+		},
+		{
+			path: "/login",
+			name: "Login",
+			component: () => import("./views/Login.vue"),
+		},
+		{
+			path: "/register",
+			name: "Register",
+			component: () => import("./views/Register.vue"),
+		},
+	],
 });
 
 router.beforeEach((to, from, next) => {
-  const user = store.getters["user/getUser"];
-  if (to.meta.requireAuth && !user) next({ name: "Login" });
-  if (to.name === "Login" && user) next({ path: from.path });
-  next();
+	const user = store.getters["user/getUser"];
+	if (to.meta.requireAuth && !user) next({ name: "Login" });
+	if (to.name === "Login" && user) next({ path: from.path });
+	next();
 });
 
 export { router };
